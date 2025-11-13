203dcf67b6aa9019596b3c5f769ab613a55ed25f
suganthan <suganthanm2005@gmail.com>
2025-11-13 15:01:13 +0530

    updated

--- src/com/tnsif/ifet/ghdemo/Demo.java
+++ src/com/tnsif/ifet/ghdemo/Demo.java
@@ -4,7 +4,8 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Raja raja chozhan naan");
+		System.out.println("Raja raja chozhan sugu than......");
+		System.out.println("dheena");
 	}
 
 }
